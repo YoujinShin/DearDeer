2a664039cdcff90edb122cf95aac5b38e20edfeb
Youjin Shin <meggonagul@gmail.com>
2015-06-08 00:55:57 -0400

    edit

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,14 @@
 // WRITING POEM
-var poem = "MY POEM<br><br>by Rich Accetta-Evans<br><br>My poem delights me.<br>My poem is mine.<br><br>It is like fresh bread<br>When I bake it myself.<br><br>It is like my son<br>The day he was born.<br><br>I laughed as I wrote it,<br>And clapped my hands.<br><br>I will stop a stranger<br>To show him my poem.<br><br>Because it delights me.<br>Because it is mine.<br><br>September 11, 1993.";
+// var poem = "MY POEM<br><br>by Rich Accetta-Evans<br><br>My poem delights me.<br>My poem is mine.<br><br>It is like fresh bread<br>When I bake it myself.<br><br>It is like my son<br>The day he was born.<br><br>I laughed as I wrote it,<br>And clapped my hands.<br><br>I will stop a stranger<br>To show him my poem.<br><br>Because it delights me.<br>Because it is mine.<br><br>September 11, 1993.";
+var poem = "MY POEM<br><br>My poem delights me.<br>My poem is mine.<br><br>It is like fresh bread<br>When I bake it myself.<br><br>It is like my son<br>The day he was born.<br><br>I laughed as I wrote it,<br>And clapped my hands.<br><br>I will stop a stranger<br>To show him my poem.<br><br>Because it delights me.<br>Because it is mine.";
+
 
 $(function(){
 
     $("#typed").typed({
     	strings: [poem],
         // strings: ["Dear Deer <br> Dear Deer"],
-        typeSpeed: 80,
+        typeSpeed: 40,
         backDelay: 500,
         loop: false,
         contentType: 'html', // or text // defaults to false for infinite loop
@@ -21,7 +23,7 @@ $(document).ready(function() {
 	$('#fullpage').fullpage({
 		// anchors: ['', '', ''],
 		anchors: ['firstPage', 'secondPage', '3rdPage'],
-		sectionsColor: ['#fff', '#000', '#fff'],
+		sectionsColor: ['#fff', '#fff', '#fff'],
 		// sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C'],
 		navigation: true,
 		navigationPosition: 'right',
